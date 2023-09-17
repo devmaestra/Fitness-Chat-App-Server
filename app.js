61c58b59b27096607573b143c605bf63e3579064
devmaestra <127052615+devmaestra@users.noreply.github.com>
2023-09-17 12:42:07 -0500

    MongoDB Connected

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const MONGO = process.env.MONGO;
 const app = express();
 const PORT = process.env.PORT || 4001;
-
 const cors = require('cors');
+
 
 
 
@@ -12,12 +14,12 @@ const cors = require('cors');
 
 // MIDDLEWARE
 const log = console.log;
+mongoose.connect (`${MONGO}/FitnessApp`);
+const db = mongoose.connection;
+db.once("open", () => log(`Connect: ${MONGO}`));
+
 app.use(express.json());
 app.use(cors());
-
-
-
-
 
 // ROUTES
 
