c1fb7d0a133a19d06d2f29a2ecc15f1c10c937a2
devmaestra <127052615+devmaestra@users.noreply.github.com>
2023-10-10 18:33:18 -0500

    activityBio added to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,54 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    locationZip: {
-        type: String,
-        required: true
-    },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  locationZip: {
+    type: String,
+    required: true,
+  },
 
-    firstName: {        // internally collected only
-        type: String,
-        required: false,
-    },
-    lastName: {        // internally collected only
-        type: String,
-        required: false,
-    },
-    bioTagline: {
-        type: String,
-    },
-    bioParagraph : {
-        type: String
-    },
-    userImage : {
-        type: String //! or and Array of Strings pointing to URLs
-    },
-    friends : {
-        type: [String] //! array of ObjectId or username of all conversations?
-    },
-    active : { // change to "visible" ?
-        type: Boolean,
-        required: true
-    },
-    admin : { // change to "visible" ?
-        type: Boolean,
-        required: true
-    }
-    
+  firstName: {
+    // internally collected only
+    type: String,
+    required: false,
+  },
+  lastName: {
+    // internally collected only
+    type: String,
+    required: false,
+  },
+  activityBio: {
+    type: String,
+    required: false,
+  },
+  bioParagraph: {
+    type: String,
+  },
+  userImage: {
+    type: String, //! or and Array of Strings pointing to URLs
+  },
+  friends: {
+    type: [String], //! array of ObjectId or username of all conversations?
+  },
+  active: {
+    // change to "visible" ?
+    type: Boolean,
+    required: true,
+  },
+  admin: {
+    // change to "visible" ?
+    type: Boolean,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
