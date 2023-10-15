4f932a250b0e077d93fe82ac4a7739e776915a71
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-10-15 14:51:16 -0400

    Created endpoint in user controller to GET One User by ID

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -403,25 +403,6 @@ router.get('/matches', validateSession, async (req, res) => {
     }
 });
 
-//! WORK IN PROGRESS FOR FRONT END GET TARGETUSER BY TARGET_ID
-//! On the frontend, you can make a request to /api/users/:target_Id with the target_Id to retrieve the target user's details, including their username.
-
-//TODO Backend API endpoint to get target user details by target_Id
-// app.get('/api/users/:target_Id', (req, res) => {
-//     const { target_Id } = req.params;
-
-//     // Fetch target user's details from your User model
-//     User.findOne({ _id: target_Id })
-//         .then(targetUser => {
-//             // Respond with the target user's details
-//             res.json(targetUser);
-//         })
-//         .catch(err => {
-//             // Handle errors if user not found, etc.
-//             res.status(500).json({ error: err.message });
-//         });
-// });
-
 
 module.exports = router;
 
